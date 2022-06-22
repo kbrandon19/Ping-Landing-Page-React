ce8ca3ac8deacb347c7270310aa3aa71cc8273b7
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-06-21 21:25:01 -0500

    adding content and styling to Title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom'
 import './index.css'
-import './Components/Title'
-import './Components/Email'
-import './Components/Image'
-import './Components/Footer'
+import Title from './Components/Title/Title'
+import Email from './Components/Email/Email'
+import Image from './Components/Image/Image'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Title />
   )
 }
 
--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom'
+import './Title.css'
+import Logo from '../../assets/logo.svg'
+
 
 
 
 function Title() {
   return (
-    <div>Title</div>
+    <div className="container">
+      <div className="logo"><img src={Logo} alt="" /></div>
+      <h1>We are launching <strong>soon!</strong></h1>
+      <p>Subscribe and get notified</p>
+    </div>
   )
 }
 
